0a5a0e1f9f7a18e7183095f0a4913989efa28679
Mauricio Santaella Ochoa <mao8a87@gmail.com>
2021-03-24 20:13:49 -0500

    boostrap added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './styles/_styles.sass'
 import store from './store'
