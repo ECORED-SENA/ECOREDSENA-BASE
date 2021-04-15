80fe839cd35371abe2b462e470325b003c05b2b1
Mauricio Santaella Ochoa <mao8a87@gmail.com>
2021-04-15 18:33:46 -0500

    Update globalMixins.js

--- src/mixins/globalMixins.js
+++ src/mixins/globalMixins.js
@@ -3,7 +3,7 @@ Vue.mixin({
   methods: {
     obtenerLink(ruta) {
       const route = window.location.href
-      let separator = route.includes('/index.html#') ? '/index.html#' : '#/'
+      let separator = route.includes('/index.html#') ? 'index.html#' : '#/'
       const baseRoute = route.split(separator)[0]
       return baseRoute + ruta
     },
