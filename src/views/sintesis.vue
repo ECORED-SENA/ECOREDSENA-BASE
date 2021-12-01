85110cab6d49c574951e7f8849044dbaca22210a
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-12-01 10:17:04 -0500

    Ejemplo Sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -9,6 +9,23 @@
           i(style="font-size: 18pt;").fas.fa-sitemap
       h1 Síntesis 
 
+    p.mb-5 Con el diagrama de actividades se termina la descripción de las tres técnicas usadas para la especificación y el análisis de requisitos, como se pudo analizar cada una de las técnicas nos brinda características para lograr representar gráficamente y de una forma clara, los requisitos que un usuario necesita poder cumplir satisfactoriamente con las solicitudes de los clientes y usuarios, por otro lado se pueden usar todas las técnicas en una especificación y análisis de requisitos si se ve la necesidad.
+      br
+      br
+      |A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+
+    .row.justify-content-center
+      .col-lg-10.mb-5
+        figure
+          img(src="@/assets/curso/sintesis.svg", alt="alt").m-auto
+      .col-auto
+        a.anexo.mb-4(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-pdf.svg")
+          .anexo__texto
+            p Anexo. Síntesis
+
+
     
     
   
