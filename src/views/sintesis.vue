e6534c384aea6716545b3edfa3391305febf5620
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-12-01 09:41:10 -0500

    Síntesis

---
+++ src/views/sintesis.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span
+          i(style="font-size: 18pt;").fas.fa-sitemap
+      h1 Síntesis 
+
+    
+    
+  
+</template>
+
+<script>
+export default {
+  name: 'Sintesis',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
