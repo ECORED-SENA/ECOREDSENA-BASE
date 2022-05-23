1264121eceeec946924f53389965b244ae1a0146
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2022-05-23 17:45:00 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -232,7 +232,7 @@ export default {
     subMenu: [
       {
         icono: 'fas fa-sitemap',
-        titulo: 'Sintesis',
+        titulo: 'Síntesis',
         nombreRuta: 'sintesis',
       },
       {
